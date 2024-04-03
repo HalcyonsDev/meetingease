3831ba1f8bb85f55f4c2357014081d014772735c
halcyon <halcyon.ruslan@gmail.com>
2024-04-03 21:45:44 +0300

    init

---
+++ src/main/java/ru/halcyon/meetingease/MeetingeaseApplication.java
@@ -0,0 +1,13 @@
+package ru.halcyon.meetingease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingeaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingeaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/halcyon/meetingease/MeetingeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.halcyon.meetingease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingeaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
