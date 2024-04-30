cc56c29fa467e0f6d77dc04c6371fd2c10973fd1
HalcyonsDev <halcyon.ruslan@gmail.com>
2024-04-30 16:58:35 +0300

    added matchers for open api

--- src/main/java/ru/halcyon/meetingease/config/SecurityConfig.java
+++ src/main/java/ru/halcyon/meetingease/config/SecurityConfig.java
@@ -33,7 +33,18 @@ public class SecurityConfig {
                                 // Entry points
                                 .requestMatchers(
                                         "/api/v1/clients/auth/**",
-                                        "/api/v1/agents/auth/**"
+                                        "/api/v1/agents/auth/**",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger/**",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
